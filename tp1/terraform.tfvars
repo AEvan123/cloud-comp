0dc4ea6c2b2f604526786654803f7fcd78f7c841
AEvan123 <137522643+AEvan123@users.noreply.github.com>
2025-09-15 09:23:33 +0100

    Add files via upload

---
+++ tp1/terraform.tfvars
@@ -0,0 +1,5 @@
+resource_group_name = "tp2"
+admin_username = "azureuser"
+public_key_path = <key_path>
+location = "UK South"
+subscription_id = <sub_id>
---
+++ tp1/variables.tf
@@ -0,0 +1,32 @@
+# variables.tf
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  default     = "East US"
+  description = "Azure region"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin username for the VM"
+}
+
+variable "public_key_path" {
+  type        = string
+  description = "Path to your SSH public key"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "ip_address" {
+  type        = string
+  description = "adresse ip de l'appareil"
+}
