991e894c0c5ef700cd9bdecbb62930eb89ac1abf
AEvan123 <137522643+AEvan123@users.noreply.github.com>
2025-09-25 01:46:47 +0200

    Update outputs.tf

--- tp3/outputs.tf
+++ tp3/outputs.tf
@@ -3,11 +3,12 @@
 # DNS
 output "dns_name" {
     description = "nom de DNS"
-    value       = azurerm_public_ip.main.fqdn
+    value       = azurerm_public_ip.vm_pip1.fqdn
 }
 
 # DNS
 output "username" {
     description = "nom d'utilisateur DB"
     value       = var.db_user
+
 }
